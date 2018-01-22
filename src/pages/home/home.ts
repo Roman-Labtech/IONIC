ba842db2cf8727ce46cf1583b2394a310e14855d
Roman-Labtech <labtechno.roman@yandex.ru>
2018-01-22 16:28:16 +0300

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+ 
+
+interface AdMobType {
+	banner: string
+}
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
-  }
-
+constructor(public navCtrl: NavController, public admob: AdMobFree) {
+ 
+    }
+ 
+    showBanner() {
+ 
+        let bannerConfig: AdMobFreeBannerConfig = {
+            isTesting: true, // Remove in production
+            autoShow: true
+            id: ca-app-pub-3940256099942544/6300978111
+        };
+ 
+        this.admob.banner.config(bannerConfig);
+ 
+        this.admob.banner.prepare().then(() => {
+            // success
+        }).catch(e => console.log(e));
+ 
+    }
+ 
 }
