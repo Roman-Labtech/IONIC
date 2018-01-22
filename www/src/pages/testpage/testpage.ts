392267bc9aa21fa0d55b11b9b3f38b2b5ea6e40c
Roman-Labtech <labtechno.roman@yandex.ru>
2018-01-22 14:52:25 +0300

    Add files via upload

---
+++ www/src/pages/testpage/testpage.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-testpage',
+  templateUrl: 'testpage.html'
+})
+export class ContactPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
